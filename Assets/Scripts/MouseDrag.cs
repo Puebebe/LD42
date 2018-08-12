bf94b3d7b4fe7d948005af2d5889cce2febe7e6a
Unknown <kawaxbart@gmail.com>
2018-08-12 11:13:19 +0200

    Mouse dragging

---
+++ Assets/Scripts/MouseDrag.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseDrag : MonoBehaviour {
+
+	float distance = 10;
+
+	[SerializeField]
+	[Range(1,20)]
+	int sensitivity = 10;
+
+
+	void Start(){
+		Cursor.lockState = CursorLockMode.Confined;
+		// POWYŻSZA LINIJKA MOŻE BYĆ BUGGOGENNA!!!!!!
+	}
+
+	void OnMouseDrag(){
+
+		Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
+
+		Vector3 destination = Camera.main.ScreenToWorldPoint(mousePosition);
+
+
+
+		GetComponent<Rigidbody>().velocity = (destination - transform.position) * sensitivity;
+
+	}
+
+
+
+
+
+
+}
