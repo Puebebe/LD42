451af028d12d98b474d016c40ba667505fa18b98
Unknown <kawaxbart@gmail.com>
2018-08-12 17:50:31 +0200

    Win conditions

---
+++ Assets/Scripts/StackAreaRender.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StackAreaRender : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		GameObject SolidArea = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+		Vector3 Scale = GameObject.Find("Shapes").GetComponent<CubeGenerator>().Dimensions;
+		SolidArea.transform.SetParent(gameObject.transform);
+		transform.localScale = new Vector3 (Scale.x*2, 5, Scale.z*2);
+		transform.localPosition = new Vector3 (transform.localPosition.x, transform.localPosition.y -2.5f, transform.localPosition.z);
+
+
+
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ Assets/Scripts/TriggerAreaBehaviour.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerAreaBehaviour : MonoBehaviour
+{
+
+    int goalCubes;
+    int actualCubes = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        goalCubes = GameObject.Find("Shapes").GetComponent<CubeGenerator>().NumberOfCubes;
+        BoxCollider boxCollider = gameObject.AddComponent<BoxCollider>();
+        boxCollider.isTrigger = true;
+
+        Vector3 Scale = GameObject.Find("Shapes").GetComponent<CubeGenerator>().Dimensions;
+        transform.localScale = Scale;
+        transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.x + Scale.y / 2, transform.localPosition.z);
+
+
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name != "Cube")
+            return;
+        
+        actualCubes++;
+        if (actualCubes == goalCubes)
+            Debug.Log("Win Faggot!");
+
+        Debug.Log(actualCubes + "/" + goalCubes);
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name != "Cube")
+            return;
+
+        actualCubes--;
+
+        Debug.Log(actualCubes + "/" + goalCubes);
+    }
+    
+
+}
